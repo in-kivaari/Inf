0efb7472cdd572d3e05e98bd0c59307782499e0d
Абаканова Валерия Артуровна <abakanova.va@edu.spbstu.ru>
2026-03-05 21:40:59 +0300

    Update bmi.sh

--- projects_3/task_3_8/bmi.sh
+++ projects_3/task_3_8/bmi.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-read -p "Введите ваш вес (в кг): " weigh
+read -p "Введите ваш вес (в кг): " weight
 read -p "Введите ваш рост(в сантиметрах): " high
 
-bmi=$(((weigh*10000)/(high * high)))
+bmi=$(((weight*10000)/(high * high)))
 
 echo "Ваш индекс массы тела составляет: $bmi"
